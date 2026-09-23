3d4b8c9e1cd93ed00e0ab0f0ffcacece0130f9d4
Ishant Yadav <142963082+ishantyadav7@users.noreply.github.com>
2026-09-23 12:13:08 +0530

    Time: 5 ms (76.34%) | Memory: 46.4 MB (69.34%) - LeetSync

---
+++ 32-longest-valid-parentheses/longest-valid-parentheses.java
@@ -0,0 +1,32 @@
+import java.util.*;
+
+class Solution {
+    public int longestValidParentheses(String s) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(-1);
+
+        int maxLength = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+
+            if (s.charAt(i) == '(') {
+                stack.push(i);
+            } else {
+                stack.pop();
+
+                if (stack.isEmpty()) {
+                    // Current ')' cannot be matched
+                    stack.push(i);
+                } else {
+                    // Length of current valid substring
+                    maxLength = Math.max(
+                        maxLength,
+                        i - stack.peek()
+                    );
+                }
+            }
+        }
+
+        return maxLength;
+    }
+}
