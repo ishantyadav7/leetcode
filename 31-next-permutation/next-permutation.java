a4502a33f9cfcf6a693ec5188b4c18b3d889ad02
Ishant Yadav <142963082+ishantyadav7@users.noreply.github.com>
2026-09-23 12:12:00 +0530

    Time: 0 ms (100.00%) | Memory: 44.6 MB (69.34%) - LeetSync

---
+++ 31-next-permutation/next-permutation.java
@@ -0,0 +1,42 @@
+class Solution {
+    public void nextPermutation(int[] nums) {
+        if (nums == null || nums.length <= 1) {
+            return;
+        }
+
+        int n = nums.length;
+        int i = n - 2;
+
+        // Step 1: Find the first decreasing element from the right
+        while (i >= 0 && nums[i] >= nums[i + 1]) {
+            i--;
+        }
+
+        // Step 2: If a valid pivot was found, find the element just larger than nums[i]
+        if (i >= 0) {
+            int j = n - 1;
+            while (nums[j] <= nums[i]) {
+                j--;
+            }
+            // Swap pivot with the next larger element
+            swap(nums, i, j);
+        }
+
+        // Step 3: Reverse the sequence from index i + 1 to the end
+        reverse(nums, i + 1, n - 1);
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+
+    private void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            swap(nums, start, end);
+            start++;
+            end--;
+        }
+    }
+}
